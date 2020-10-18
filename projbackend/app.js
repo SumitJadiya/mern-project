cd710a0ffe3b440591b59877adf604451d68e61b
Sumit Jadiya <sumit.jadiya@gmail.com>
2020-10-18 17:18:29 +0530

    add git ignore

--- projbackend/app.js
+++ projbackend/app.js
@@ -6,17 +6,17 @@ const mongoose = require("mongoose");
 const app = express();
 
 mongoose
-  .connect(process.env.DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log("DB connected");
-  });
+    .connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then(() => {
+        console.log("DB connected")
+    })
 
 const port = process.env.DATABASE_PORT;
 
 app.listen(port, () => {
-  console.log(`app is running at ${port}`);
+    console.log(`app is running at ${port}`)
 });
